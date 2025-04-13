50e5f8ac0a3f6c4e2e8c98ec92a9007a73365158
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-04-13 18:19:47 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Make sure Vite listens on all interfaces
-    port: 5173,       // Use the desired port (change if needed)
-  }
 })
