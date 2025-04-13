2411d781fcf3f66cfa65e847811df7efe2a440d3
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-04-13 18:42:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000,  // Fallback to port 3000 if $PORT is not available
+    host: '0.0.0.0',  // Ensure the server is accessible externally
+  },
+});
