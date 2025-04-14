52fe188ffb3419f074158af10a8f59ca31c760f0
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-04-14 09:20:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
 import userRouter from './routes/userRoute.js'
@@ -11,13 +12,11 @@ dotenv.config();
 // App Config
 const app = express()
 const port = 4000
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json())
 app.use(cors())
-
-// DB Connection
-connectDB();
 
 // api endpoint
 app.use('/api/food', foodRouter)
@@ -33,7 +32,8 @@ app.get("*", (req, res) => {
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
+	// DB Connection
+	connectDB();
 	console.log(`Server Running on port ${PORT}`);
 });
 
